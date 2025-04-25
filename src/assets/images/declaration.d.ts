6d6dac2c3e52da3dc69466bb6d4e644468664ced
JosielLinhar <iamlinhar@gmail.com>
2025-04-24 22:18:31 -0300

    criação de assets e parametrização de config

---
+++ src/assets/images/declaration.d.ts
@@ -0,0 +1,12 @@
+declare module '*.svg' {
+  import React from 'react';
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export { ReactComponent };
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
